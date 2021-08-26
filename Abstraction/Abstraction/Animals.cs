6508c5d13364cf6a89c1cdf84013a89a1928798e
OJ <89594218+OJ-1@users.noreply.github.com>
2021-08-26 17:08:28 +0200

    Add files via upload

---
+++ Abstraction/Abstraction/Animals.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstraction
+{
+    public abstract class Animals
+    {
+        public abstract void eat();
+
+        public void sound()
+        {
+            Console.WriteLine("Animals make funny sounds");
+        }
+
+
+    }
+
+    class Dog : Animals
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Some animals eat at night");
+        }
+    }
+}
---
+++ Abstraction/Abstraction/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals myPet = new Dog();
+
+            //Dog dg = new Dog();
+
+            myPet.sound();
+            myPet.eat();
+        }
+    }
+}
